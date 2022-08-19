1669a54ee247cd35446153f967f63cce7e2c7682
Leandro <leandrobotali@gmail.com>
2022-08-19 15:33:15 -0300

    cambios en readme

--- README.md
+++ README.md
@@ -2,12 +2,22 @@
 
 ## Run en localhost:
 
-  $ git clone https://github.com/leandrobotali/Tercer-Entrega.git
+  $ git clone https://github.com/leandrobotali/Proyecto-Final.git
   
   $ npm i
+
+  $set environment variables:
+  MONGO_URI_LOCAL (default mongodb://localhost:27017)
+  MONGO_URI_ATLAS 
+  NODE_ENV (default dev)
+  PRIVATE_KEY
+  EMAIL_ADMIN 
+  ETHEREAL_EMAIL_NAME
+  ETHEREAL_EMAIL_USERNAME
+  ETHEREAL_EMAIL_PASSWORD
   
   $ npm run dev
 
 ## Deploy en heroku:
 
-  https://tercer-entrega-pro.herokuapp.com/
+  https://proyectofinal-bk.herokuapp.com/
--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,14 @@ import 'dotenv/config'
 
 const config = {
     URLmogoAtlas: process.env.MONGO_URI_ATLAS,//url de conexion de mongo atlas
-    URLmongoLocal: 'mongodb://localhost:27017',//url de conexion de mongo local
+    URLmongoLocal: process.env.MONGO_URI_LOCAL || 'mongodb://localhost:27017',//url de conexion de mongo local
     private_key: process.env.PRIVATE_KEY || "myprivatekey",//key de encriptacion  
     adminEmail: process.env.EMAIL_ADMIN || "admin@admin.com",//email de admin
     entorno: process.env.NODE_ENV || 'dev',//entorno de ejecucion
     ethereal: {//datos de cuenta de ethereal   
-        Name: "Nyasia Koelpin",
-        Username: "nyasia11@ethereal.email",
-        Password: "YBy18gmxSZVkeQrmft"
+        Name: process.env.ETHEREAL_EMAIL_NAME,
+        Username: process.env.ETHEREAL_EMAIL_USERNAME,
+        Password: process.env.ETHEREAL_EMAIL_PASSWORD
     }
 }
 
